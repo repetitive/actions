47baebc5df95471e0870a66914ff8a5c13343559
podviaznikov <anton@podviaznikov.com>
2019-01-27 17:10:59 -0800

    post a comment

--- clojure-repl/entrypoint.sh
+++ clojure-repl/entrypoint.sh
@@ -15,23 +15,10 @@ if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
 	exit 78
 fi
 
-PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-echo "Collecting information about PR #$PR_NUMBER of $REPO_FULLNAME..."
-
 if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "Set the GITHUB_TOKEN env variable."
 	exit 1
 fi
-
-URI=https://api.github.com
-API_HEADER="Accept: application/vnd.github.v3+json"
-AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
-
-pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
-
-
 
 # Evaluate comment and capture output
 echo "Evaluate comment and capture output"
@@ -39,3 +26,18 @@ output=$(clojure --eval "(+ 1 2 3)")
 
 # Write output to STDOUT
 echo "$output"
+
+ISSUE_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+echo "Creating new comment #$ISSUE_NUMBER of $REPO_FULLNAME..."
+
+
+URI=https://api.github.com
+API_HEADER="Accept: application/vnd.github.v3+json"
+AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+
+
+new_comment_resp=$(curl --data "{\"body\": \"```$output```\"}" -X POST -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
+          "${URI}/repos/$REPO_FULLNAME/issues/$ISSUE_NUMBER/comments")
+
+
