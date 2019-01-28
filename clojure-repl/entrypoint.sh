d69c841bccec148eca9b6c4861ee231c88f329c4
podviaznikov <anton@podviaznikov.com>
2019-01-27 17:21:11 -0800

    updates

--- clojure-repl/entrypoint.sh
+++ clojure-repl/entrypoint.sh
@@ -6,10 +6,6 @@ set -e
 echo "Checking if contains '/repl' command..."
 (jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -E "/repl") || exit 78
 
-# skip if not a PR
-echo "Checking if a PR command..."
-(jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
-
 if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
 	echo "This is not a new comment event!"
 	exit 78
@@ -20,8 +16,11 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	exit 1
 fi
 
+comment_body=(jq -r ".issue.body" "$GITHUB_EVENT_PATH")
+
 # Evaluate comment and capture output
-echo "Evaluate comment and capture output"
+echo "Evaluate comment and capture output:"
+echo comment_body 
 output=$(clojure --eval "(+ 1 2 3)")
 
 # Write output to STDOUT
@@ -31,12 +30,10 @@ ISSUE_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 echo "Creating new comment #$ISSUE_NUMBER of $REPO_FULLNAME..."
 
-
-URI=https://api.github.com
+COMMENTS_URI=$(jq -r ".issue.comments_url" "$GITHUB_EVENT_PATH")
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 
-new_comment_resp=$(curl --data "{\"body\": \"```data```\"}" -X POST -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$REPO_FULLNAME/issues/$ISSUE_NUMBER/comments")
+new_comment_resp=$(curl --data "{\"body\": \"```data```\"}" -X POST -s -H "${AUTH_HEADER}" -H "${API_HEADER}" ${COMMENTS_URI})
 
 
